a1b1d42ffbae2655659f5d431cc1611384816df6
yongs <smartdragon417@gmail.com>
2020-09-09 19:54:20 +0900

    Add files via upload
    todat servlet 구현
    메인페이지 화면전환 구현

--- aboutme/src/TodayServlet.java
+++ aboutme/src/TodayServlet.java
@@ -2,6 +2,8 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -10,9 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class TodayServlet
- */
+
 @WebServlet("/TodayServlet")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -24,18 +24,23 @@ public class TodayServlet extends HttpServlet {
 
     
     public void init(ServletConfig config) throws ServletException {
-		System.out.println("init test È£Ãâ!!");
+		System.out.println("init test!!");
 	}
 
 	
 	public void destroy() {
-		System.out.println("destroy È£Ãâ!!");
+		System.out.println("destroy !!");
 	}
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/M/d HH:mm");
+		String formatDateTime = now.format(formatter);
 		
-		response.setContentType("text/html");
+		
+		
+		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<head><title>form</title></head>");
@@ -44,9 +49,11 @@ public class TodayServlet extends HttpServlet {
 		out.println("name : <input type='text' name='name'><br>");
 		out.println("<a href='index.html'");
 		out.println("<li>main page");
-		
 	    out.println("</li>");
 		out.println("</a>");
+		out.println("<li>í˜„ìž¬ì‹œê°„:" + formatDateTime);
+		out.println("</li>");
+		
 		out.println("<input type='submit' value='ok'><br>");                                                 
 		out.println("</form>");
 		out.println("</body>");
@@ -62,7 +69,7 @@ public class TodayServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		response.setContentType("text/html");
+		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		String name = request.getParameter("name");
 		out.println("<h1> hello " + name + "</h1>");
